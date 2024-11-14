1b6f1f2de86a7025a4738aa78e324d2756dc6d25
rafael565 <147094251+rafael565@users.noreply.github.com>
2024-11-14 10:21:59 -0300

    Controler criado

---
+++ Controllers/DadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoFundacaoCrianca.Controllers
+{
+    public class DadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
